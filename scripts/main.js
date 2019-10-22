edf577f6beb5b512b890aac142e2a2d3d1280298
Veronika D <donets.veronika@yahoo.com>
2019-10-23 01:13:41 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+if (!localStorage.hasOwnProperty('clickAmount')) {
+  localStorage.setItem('spentTime', 0);
+}
+
+document.getElementById('timer').innerHTML
+  = `You spent ${localStorage.spentTime} sec on current page`;
+
+setInterval(function() {
+  localStorage.spentTime++;
+  document.getElementById('timer').innerHTML
+    = `You spent ${localStorage.spentTime} sec on current page`;
+}, 1000);
