2a38d5229f29d62dab337f58a375d2c63ac348ef
Veronika D <donets.veronika@yahoo.com>
2019-10-23 01:11:00 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+if (!localStorage.hasOwnProperty('clickAmount')) {
+  localStorage.setItem('spentTime', 0);
+}
+document.getElementById('timer').innerHTML
+  = `You spent ${localStorage.spentTime} sec on current page`;
+
+setInterval(function() {
+  localStorage.spentTime++;
+  document.getElementById('timer').innerHTML
+    = `You spent ${localStorage.spentTime} sec on current page`;
+}, 1000);
